59f1f0a229ac1534360c50aaa462aa2a37909c12
batro83 <batro_ska@yahoo.es>
2020-02-24 17:15:21 +0100

    seed

---
+++ src/main/java/com/app/zitrogames/ZitrogamesApplication.java
@@ -0,0 +1,13 @@
+package com.app.zitrogames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZitrogamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZitrogamesApplication.class, args);
+	}
+
+}
